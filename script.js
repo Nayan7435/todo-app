35c0c092743deeed5880c266ef88a17374f4d86d
Gohel Nayan <gohelnayan264@gmail.com>
2026-05-08 15:37:39 +0530

    fixed delete from localStorage

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+savedTasks.forEach(function (taskText) {
+  let ul = document.getElementById("taskList");
+  let li = document.createElement("li");
+  let deleteBtn = document.createElement("button");
+
+  li.innerText = taskText;
+  deleteBtn.innerText = "❌";
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let index = tasks.indexOf(taskText);
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+});
+
 let button = document.getElementById("addBtn");
 
-button.addEventListener("click", function() {
-    let input = document.getElementById("forInput");
-    let ul = document.getElementById("taskList");
+button.addEventListener("click", function () {
+  let input = document.getElementById("forInput");
+  let taskText = input.value;
+  let ul = document.getElementById("taskList");
 
-    let li = document.createElement("li");
-    let deleteBtn = document.createElement("button");
+  let li = document.createElement("li");
+  let deleteBtn = document.createElement("button");
 
-    li.innerText = input.value;
-    deleteBtn.innerText = "❌";
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(input.value);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    deleteBtn.addEventListener("click", function() {
-        // ul.removeChild(li);
-        li.remove();
-    });
+  li.innerText = input.value;
+  deleteBtn.innerText = "❌";
 
-    li.appendChild(deleteBtn);
-    ul.appendChild(li);
-    input.value = "";
+  deleteBtn.addEventListener("click", function () {
+    // ul.removeChild(li);
+    li.remove();
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let index = tasks.indexOf(taskText);
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+  input.value = "";
+
+  li.addEventListener("click", function () {
+    if (li.style.textDecoration === "line-through") {
+      li.style.textDecoration = "none";
+    } else {
+      li.style.textDecoration = "line-through";
+    }
+  });
 });
